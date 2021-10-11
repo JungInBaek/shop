b765f9a53fd57e23f9dd7fcd0083bc9451b4387b
JungIn <bji1527@naver.com>
2021-10-11 19:54:17 +0900

    쿼리 메소드 작성, 테스트
    쿼리 메소드로 조회 및 테스트 진행

--- src/main/java/com/shop/Repository/ItemRepository.java
+++ src/main/java/com/shop/Repository/ItemRepository.java
@@ -3,7 +3,16 @@ package com.shop.Repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    List<Item> findByItemName(String itemName);
+
+    List<Item> findByItemNameOrItemDetail(String itemName, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 }
