2af6c49355d572f07fc6eed8b8d76d805ed92686
JungInBaek <bji1527@naver.com>
2021-10-20 00:14:16 +0900

    상품 등록 기능 구현

--- src/main/java/com/shop/dto/ItemDto.java
+++ src/main/java/com/shop/dto/ItemDto.java
@@ -10,10 +10,16 @@ import java.time.LocalDateTime;
 public class ItemDto {
 
     private Long id;
+
     private String itemName;
+
     private Integer price;
+
     private String itemDetail;
+
     private String sellStatCd;
+
     private LocalDateTime regTime;
+
     private LocalDateTime updateTime;
 }
