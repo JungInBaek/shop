a01ed40a6bb2d87fe9f1042abf0c3989659e1387
JungInBaek <bji1527@naver.com>
2021-10-22 02:25:22 +0900

    메인 화면 완성
    메인화면 상품 검색 기능 구현
    메인화면 상품 조회 기능 구현

--- src/test/java/com/shop/entity/MemberTest.java
+++ src/test/java/com/shop/entity/MemberTest.java
@@ -40,7 +40,7 @@ public class MemberTest {
 
         System.out.println("register time : " + member.getRegTime());
         System.out.println("update time : " + member.getUpdateTime());
-        System.out.println("create member : " + member.getCreateBy());
+        System.out.println("create member : " + member.getCreatedBy());
         System.out.println("modified member : " + member.getModifiedBy());
     }
 }
